f182feb641f1fef385fe4493d76ac36d9f637d62
coon <victorycoon@gmail.com>
2025-10-27 23:21:05 +0900

    fix:주석 추가

--- tokenizer/BPETokenizer.py
+++ tokenizer/BPETokenizer.py
@@ -13,7 +13,7 @@ class BPETokenizer:
             self.special_token = special_token
 
     def get_stats(self, vocab):
-        pairs = defaultdict(int)
+        pairs = defaultdict(int) # 23개가 나오는 근거는??? 46음절의 절반수치다.
         for word, freq in vocab.items():
             symbols = word.split()
             for i in range(len(symbols) - 1):
@@ -27,41 +27,48 @@ class BPETokenizer:
         pattern = re.compile(r'(?<!\S)' + bigram + r'(?!\S)')
         v_out = {}
         for word in v_in:
+            # pair 에 해당되는 v_in은 공백을 제거해서 읽는다.
             w_out = pattern.sub(''.join(pair), word)
             v_out[w_out] = v_in[word]
+            # Dictionary라서 그런가?
         return v_out
 
     def train(self, corpus, num_merges=100):
         vocab = Counter(corpus.split())
         self.vocab = {' '.join(word) + ' </w>': freq for word, freq in vocab.items()}
         #tokens = Counter([' '.join(word) + ' </w>' for word in corpus])
+        #받은 단어배열을 전부 공백으로 쪼개고, 단어의 끝에 </w>를 하는 것, 그리고 빈도수 입력
         for i in range(num_merges):
             pairs = self.get_stats(self.vocab)
             if not pairs:
                 break
-            best = max(pairs, key=pairs.get)
+            best = max(pairs, key=pairs.get) #가장 좋은 결과를 합쳐서, 다시 단어반환
             self.vocab = self.merge_vocab(best, self.vocab)
-            self.merges.append(best)
+            self.merges.append(best) # self.merge의 Element는 마치, 입력,예측 관계같다.
 
         tokens = set()
-        for word in self.vocab.keys():
+        for word in self.vocab.keys(): # Key는 단어들, Values는 빈도
             tokens.update(word.split())
         #self.token_to_id = {token: idx for idx, token in enumerate(sorted(tokens))}
         #self.id_to_token = {idx: token for token, idx in self.token_to_id.items()}
         idx = 0
-        for token in self.special_token:
+        # 다시금 tokens로 인해, vocab사전과 유사한 결과를 얻었다(결과만 보고는 유사 뻘짓인데?)
+
+        for token in self.special_token: # 스페셜 토큰은 4개다.
+            # 현재까지 둘은 빈 상태였다.
             self.token_to_id[token] = idx
             self.id_to_token[idx] = token
-            idx += 1
+            idx += 1 # idx는 3(0,1,2,3)에서 끝날것이다.
 
         for token in sorted(tokens):
             if token not in self.token_to_id:
                 self.token_to_id[token] = idx
                 self.id_to_token[idx] = token
                 idx += 1
+        #학습을 끝으로 얻는것은, token_to_id, id_to_token이다.
 
     def encode_word(self, word):
-        word = list(word) + ["</w>"]
+        word = list(word) + ["</w>"] # 단어를 전부 음절로 자르고, 단어끼리는 </w>구분자를 준다.
         while True:
             pairs = [(word[i], word[i + 1]) for i in range(len(word) - 1)]
             pair_ranks = {pair: i for i, pair in enumerate(self.merges)}
@@ -81,6 +88,7 @@ class BPETokenizer:
             word = new_word
         return word
 
+    # 여기서부터는 학습으로 얻은 token_to_id, id_to_token을 사용해서, 본격 Tokenizer를 진행한다.
     def encode_sentence(self, sentence):
         encoded_sentence = [self.token_to_id['<sos>']]
         for word in sentence.strip().split():
@@ -96,15 +104,16 @@ class BPETokenizer:
         return sentence
 
 
-corpus = "low lower lowest lowly lower newest wide wider"
+#corpus = "low lower lowest lowly lower newest wide wider"
+corpus = "this these that thing those me you your my mine one"
 
 tokenizer = BPETokenizer()
 tokenizer.train(corpus, num_merges=30)
 
-sentence = "low lowest"
+sentence = "This is your thing."
 encoded = tokenizer.encode_sentence(sentence)
 decoded = tokenizer.decode_sentence(encoded)
 
-print("원문:", sentence)
-print("인코딩:", encoded)
-print("디코딩:", decoded)
+#print("원문:", sentence)
+#print("인코딩:", encoded)
+#print("디코딩:", decoded)
